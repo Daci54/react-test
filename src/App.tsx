1c02789da87b97182e6565827e615746ed614b4c
Daci54 <d.stojkovic93@gmail.com>
2022-04-03 18:12:33 +0200

    removed counter from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '@progress/kendo-theme-default/dist/all.css';
 import './App.css';
 import ProductGrid from './Components/ProductGrid/ProductGrid';
 
 function App(): JSX.Element {
-  const [count, setCounter] = useState(0);
-
   return (
     <div className='app-container'>
-      <h1>In App</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        Increase counter
-      </button>
-      <h2>{count}</h2>
       <ProductGrid />
     </div>
   );
