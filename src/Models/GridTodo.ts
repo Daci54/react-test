1b67d767b9a128eac64b62e168a9fe4a0e228448
Daci54 <d.stojkovic93@gmail.com>
2022-04-04 21:38:56 +0200

    extended grid todos with selected field

--- src/Models/GridTodo.ts
+++ src/Models/GridTodo.ts
@@ -1,7 +1,11 @@
+import { TodoStatus } from './TodoStatus';
+
 export interface GridTodo {
   userId: number;
   id: number;
   title: string;
   completed: boolean;
   created?: Date;
+  selected: boolean;
+  status: TodoStatus;
 }
