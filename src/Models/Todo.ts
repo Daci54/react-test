5225a5423d5a478a5323eca0a38ac153cd5948a6
Daci54 <d.stojkovic93@gmail.com>
2022-04-04 20:26:14 +0200

    added status model for grid todos

--- src/Models/Todo.ts
+++ src/Models/Todo.ts
@@ -1,7 +1,10 @@
+import { TodoStatus } from './TodoStatus';
+
 export interface Todo {
   userId: number;
   id: number;
   title: string;
   completed: boolean;
   created: string;
+  status: TodoStatus;
 }
