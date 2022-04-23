f300bfd118138babfa65cfceeeddb1b5686cd84c
Daci54 <d.stojkovic93@gmail.com>
2022-04-23 18:11:15 +0200

    refactored todo to grid todo conversion

--- src/Helper/Helper.ts
+++ src/Helper/Helper.ts
@@ -1,4 +1,19 @@
+import { GridTodo } from '../Models/GridTodo';
+import { Todo } from '../Models/Todo';
+
 export function generateDate(dateString: string): Date {
   const [year, month, day]: string[] = dateString.split('-');
   return new Date(+year, +month, +day);
+}
+
+export function convertTodosToGridTodos(
+  fetchedTodos?: Todo[]
+): GridTodo[] | undefined {
+  return fetchedTodos?.map((todo: Todo) => {
+    return {
+      ...todo,
+      created: todo.created ? new Date(todo.created) : null,
+      selected: false,
+    };
+  });
 }
